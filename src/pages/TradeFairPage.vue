960c303930f758987067174874381675a962b3e3
Abhishek Negi <abhishek.fpaipl@gmail.com>
2024-07-10 16:06:01 +0530

    d

--- src/pages/TradeFairPage.vue
+++ src/pages/TradeFairPage.vue
@@ -158,7 +158,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="enquirymodalLabel">Modal title</h1>
+                        <h1 class="modal-title fs-5" id="enquirymodalLabel">Enquiry</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
